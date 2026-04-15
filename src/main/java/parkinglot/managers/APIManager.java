be77fbc40058c4ad1d5ff8e7984d079b4eb1fde4
kosmo <228873266+250245-lang@users.noreply.github.com>
2026-04-15 13:35:51 +0500

    some minor additions

---
+++ src/main/java/parkinglot/managers/APIManager.java
@@ -0,0 +1,5 @@
+package parkinglot.managers;
+
+public class APIManager {
+
+}
---
+++ src/main/java/parkinglot/ui/Login.java
@@ -0,0 +1,6 @@
+package parkinglot.ui;
+
+
+// store the jwt session on save_state in plain text or binary
+public class Login {
+}
