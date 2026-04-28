0863478239f8c98faeb2bcfb01f768d83aae1ea1
KOSMOSTARuzb <94840242+KOSMOSTARuzb@users.noreply.github.com>
2026-04-28 22:52:25 +0500

    Removed server/Main.java as server now has GUI

--- src/main/java/parkinglot/server/Main.java
+++
@@ -1,9 +0,0 @@
-package parkinglot.server;
-
-import org.springframework.boot.SpringApplication;
-
-public class Main {
-    public static void main(String[] args) {
-        SpringApplication.run(ServerApplication.class, args);
-    }
-}
